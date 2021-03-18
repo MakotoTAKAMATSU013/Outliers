ca81bfe43f95e1783d84e29e0e512e218c3ce8c6
Makoto TAKAMATSU <75411019+MakotoTAKAMATSU013@users.noreply.github.com>
2021-03-18 22:34:52 +0900

    Create __init__.py

---
+++ Outliers/models/__init__.py
@@ -0,0 +1,11 @@
+from .mad import MahalanobisAD
+from .dsvdd import DeepSVDD
+#from .base import ResNet, EfficientNet
+
+__all__ = [
+    'MahalanobisAD',
+    'DeepSVDD',
+    'ResNet',
+    'EfficientNet',
+]
+
