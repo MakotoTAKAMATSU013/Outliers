b770cab75276b8ed9ddbf1acbc005eaad90f4bda
Makoto TAKAMATSU <75411019+MakotoTAKAMATSU013@users.noreply.github.com>
2021-03-18 22:35:10 +0900

    Create dsvdd.py

---
+++ Outliers/models/dsvdd.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sklearn.metrics import roc_auc_score
+from tqdm import tqdm
+
+
+class DeepSVDD:
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
