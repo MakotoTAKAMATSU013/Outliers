b5e27d3f03d012a993fa0a25e0a0b1b9c708418e
Makoto TAKAMATSU <75411019+MakotoTAKAMATSU013@users.noreply.github.com>
2021-03-18 22:35:19 +0900

    Create mad.py

---
+++ Outliers/models/mad.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as cp
+
+
+import torchvision
+
+class MahalanobisAD:
+    def __init__(self):
+        super().__init__()
+
+
